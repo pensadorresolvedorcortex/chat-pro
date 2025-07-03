677b412074caf3a911aa884164e0593edd25f1f0
pensadorresolvedorcortex <pensadorquanticoarrojado@gmail.com>
2025-07-03 10:01:52 -0300

    Add bet price setting and logs page

---
+++ bolao-x/uninstall.php
@@ -0,0 +1,20 @@
+<?php
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit();
+}
+
+$types = array( 'bolaox_aposta', 'bolaox_result' );
+$posts = get_posts( array(
+    'post_type'   => $types,
+    'numberposts' => -1,
+    'post_status' => 'any',
+) );
+foreach ( $posts as $post ) {
+    wp_delete_post( $post->ID, true );
+}
+
+delete_option( 'bolaox_result' );
+delete_option( 'bolaox_cutoffs' );
+delete_option( 'bolaox_mp_tokens' );
+delete_option( 'bolaox_mp_active' );
+
