d1eac28a900fb099f33b400218f95204024d390a
pensadorresolvedorcortex <pensadorquanticoarrojado@gmail.com>
2025-07-28 21:28:45 -0300

    Add basic PHP player draw system

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+$players = [
+    ["name" => "Dheniell"],
+    ["name" => "Dario"],
+    ["name" => "Papel"],
+    ["name" => "Wallace"],
+    ["name" => "Matheus"],
+    ["name" => "Kloh"],
+    ["name" => "Bebeto"],
+    ["name" => "Custela"],
+    ["name" => "Diego"],
+    ["name" => "Matheus MP"],
+    ["name" => "Gabriel"],
+    ["name" => "Bolo"],
+    ["name" => "Geisel"],
+    ["name" => "Caputo"],
+    ["name" => "Fred"],
+    ["name" => "Darlan"],
+    ["name" => "Baiano"],
+];
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<title>Sorteio de Times</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Sistema de Sorteio de Jogadores</h1>
+<table id="tabela-jogadores">
+<thead>
+<tr><th>Nº</th><th>Nome</th><th>Posição</th><th>Pedra</th></tr>
+</thead>
+<tbody>
+<?php foreach ($players as $i => $p): ?>
+<tr>
+<td><?= $i+1 ?></td>
+<td contenteditable="true" class="editable nome"><?= $p['name'] ?></td>
+<td>
+<select class="posicao">
+<option value="">--Selecione--</option>
+<option>Goleiro</option>
+<option>Fixo</option>
+<option>LD</option>
+<option>LE</option>
+<option>Meia</option>
+<option>Pivô</option>
+</select>
+</td>
+<td>
+<select class="pedra">
+<option value="">--Selecione--</option>
+<option value="1" class="p1">Pedra 1</option>
+<option value="2" class="p2">Pedra 2</option>
+<option value="3" class="p3">Pedra 3</option>
+</select>
+</td>
+</tr>
+<?php endforeach; ?>
+</tbody>
+</table>
+<div class="botoes">
+<button id="sortear">Sortear Times</button>
+<button id="resetar">Resetar Tabela</button>
+<button id="salvar">Salvar Times</button>
+<button id="limpar">Resetar Times Sorteados</button>
+</div>
+<div id="resultado"></div>
+<script src="script.js"></script>
+</body>
+</html>
